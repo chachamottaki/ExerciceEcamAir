2b0e7152efbd4087f4e7e2ede55e35c0b7b9dd35
Chacha <79138550+ChachaPlusUltra@users.noreply.github.com>
2023-02-19 18:44:03 +0100

    MVC structure

--- server.js
+++ server.js
@@ -5,7 +5,16 @@ let express = require('express');
 let app = express();
 
 //Send message for default URL
-app.get('/', (request, response) => response.send('Hello world !'));
+// app.get('/', (request, response) => response.send('Hello world !'));
+
+//render ejs pages
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+    res.render('reservation');
+ });
+ 
+//serve up static files (css)
+ app.use('/public', express.static('public'));
 
 //Setup server post
 let port = 8000;
