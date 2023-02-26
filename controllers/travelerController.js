dedfd4908ea45ba7acea9c093acea3079a3dbf65
Chacha <79138550+ChachaPlusUltra@users.noreply.github.com>
2023-02-26 13:45:53 +0100

    create traveler object + add to travelersList

--- controllers/travelerController.js
+++ controllers/travelerController.js
@@ -1,5 +1,13 @@
-let traveler = require("../models/travelerModel");
+let Traveler = require("../models/travelerModel");
+travelersList = [];
 
 exports.validate = function(req,res){
+    let name = req.body.name;
+    let age = req.body.age;
+
+    let traveler = new Traveler(name,age);
+    travelersList.push(traveler);
+    console.log(travelersList);
+
     res.render('validation.ejs');
 }
