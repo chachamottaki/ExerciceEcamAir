5101b36ace4ab4d6533af90445e7cbc433963943
Chacha <79138550+ChachaPlusUltra@users.noreply.github.com>
2023-02-26 12:37:44 +0100

    create order object with forminputs thx to req.body...(bodyparser

--- controllers/reservationController.js
+++ controllers/reservationController.js
@@ -1,12 +1,21 @@
 
 let order = require("../models/reservationModel");
+let reservationList = [];
 
 exports.addOrder = function(req,res){
     //let order1 = new Reservation();
     res.render('reservation.ejs');
 }
 exports.encode = function(req,res){
- 
+    let destination = req.body.destination; //get req.body. ... value in .ejs : name= ...
+    let amount = req.body.amount;
+    let insurance = req.body.insurance;
+    let price = req.body.totalPrice;
+
+    let reservation = new order(destination, amount, insurance, price);
+    reservationList.push(reservation);
+    console.log(reservationList);
+
     res.render('encode.ejs');
 }
 exports.validate = function(req,res){
